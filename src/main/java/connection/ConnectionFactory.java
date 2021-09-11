a4884f4ddb43aa28c9b5c24d44e17e799b031be9
Wellington de Oliveira Costa <wellington.costa.103@ufrn.edu.br>
2021-09-10 23:26:30 -0300

    Remoção das credenciais de acesso ao banco

--- src/main/java/connection/ConnectionFactory.java
+++ src/main/java/connection/ConnectionFactory.java
@@ -9,7 +9,7 @@ public class ConnectionFactory {
     private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
     private static final String URL = "jdbc:mysql://localhost:3306/und_3";
     private static final String USER ="root";
-    private static final String PASS ="201331mysql";
+    private static final String PASS ="";
 
     public static Connection getConnection(){
         try {
